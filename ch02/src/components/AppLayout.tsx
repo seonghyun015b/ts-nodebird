e3cb7437d0194a1bafeafe4bb15b30bde6270e54
seonghyun015b <seonghyun015b@gmail.com>
2023-08-02 18:26:55 +0900

    antd, styled-components, 설치

---
+++ ch02/src/components/AppLayout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface AppLayout {
+  children: React.ReactNode;
+}
+
+const AppLayout = ({ children }: AppLayout) => {
+  return (
+    <div>
+      <div>
+        <Link href='/'>노드버드</Link>
+        <Link href='/profile'>프로필</Link>
+        <Link href='/signup'>회원가입</Link>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default AppLayout;
