2926ea224589ad5ad3e1365e882074e564d43554
seonghyun015b <seonghyun015b@gmail.com>
2023-08-03 16:04:07 +0900

    useCallback, 로그인폼

--- ch02/src/components/AppLayout.tsx
+++ ch02/src/components/AppLayout.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Col, Input, Menu, Row } from 'antd';
+
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
 
 interface AppLayout {
   children: React.ReactNode;
 }
 
 const AppLayout = ({ children }: AppLayout) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode='horizontal'>
@@ -17,10 +22,29 @@ const AppLayout = ({ children }: AppLayout) => {
           <Link href='/profile'>프로필</Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href='/signup'>회원가입</Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href='https://www.naver.com'
+            target='_blank'
+            rel='noreferrer nooppener'
+          >
+            네이버
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
