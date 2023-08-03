eb4f52c9b0bd0c96f1e0048043e86d932a3d1ddb
seonghyun015b <seonghyun015b@gmail.com>
2023-08-03 18:08:46 +0900

    프로필 페이지

--- ch02/src/pages/profile.tsx
+++ ch02/src/pages/profile.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
+import FollowList from '../components/FollowList';
+import NicknameEditForm from '../components/NicknameEditForm';
 import Head from 'next/head';
 
 const Profile = () => {
+  const followerList = [
+    { nickname: '제로초' },
+    { nickname: '바보' },
+    { nickname: '노드버드오피셜' },
+  ];
+
+  const followingList = [
+    { nickname: '제로초' },
+    { nickname: '바보' },
+    { nickname: '노드버드오피셜' },
+  ];
+
   return (
     <>
       <Head>
         <title>프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-        <div>프로필</div>
+        <NicknameEditForm />
+        <FollowList header='팔로잉 목록' data={followingList} />
+        <FollowList header='팔로워 목록' data={followerList} />
       </AppLayout>
     </>
   );
