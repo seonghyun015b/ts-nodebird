92df59d99917167077d28b264f1ca67f2dd915bf
seonghyun015b <seonghyun015b@gmail.com>
2023-08-05 14:09:15 +0900

    이미지 컴포넌트

--- ch03/src/components/PostImages.tsx
+++ ch03/src/components/PostImages.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
 
-const PostImages = ({ images }) => {
+interface PostImages {
+  images: { src: string }[];
+}
+
+const PostImages = ({ images }: PostImages) => {
+  const [showImageZoom, setShowImageZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImageZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role='presentation'
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role='presentation'
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role='presentation'
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
   return (
     <>
-      <div>...제작중</div>
+      <div>
+        <img
+          role='presentation'
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <div
+          role='presentation'
+          style={{
+            display: 'inline-block',
+            width: '50%',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}개의 사진 더보기
+        </div>
+      </div>
     </>
   );
 };
