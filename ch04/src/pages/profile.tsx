3d8da9c6357e149383c59cf23e2795b821f0516c
seonghyun015b <seonghyun015b@gmail.com>
2023-08-07 13:54:50 +0900

    라우터

--- ch04/src/pages/profile.tsx
+++ ch04/src/pages/profile.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLayout from '../components/AppLayout';
 import FollowList from '../components/FollowList';
 import NicknameEditForm from '../components/NicknameEditForm';
 import Head from 'next/head';
 import { useSelector } from 'react-redux';
 import { RootState } from '../reducers/type';
+import Router from 'next/router';
 
 const Profile = () => {
   const { me } = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
