0d139e252ef8f978ba2e16086017884a73d071fa
seonghyun015b <seonghyun015b@gmail.com>
2023-08-09 11:36:40 +0900

    express

--- ch05/back/app.js
+++ ch05/back/app.js
@@ -1,14 +1,37 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("http");
-var server = http.createServer(function (req, res) {
-    console.log(req.url, req.method);
-    res.write('<h1>Hello Node</h1>');
-    res.write('<h2>Hello Node</h2>');
-    res.write('<h3>Hello Node</h3>');
-    res.write('<h4>Hello Node</h4>');
-    res.end('<h5>Hello node</h5>');
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+app.get('/', function (req, res) {
+    res.send('Hello express');
 });
-server.listen(3065, function () {
-    console.log('3065 포트에서 서버 대기중');
+app.get('/api', function (req, res) {
+    res.send('Hello api');
+});
+app.get('/api/posts', function (req, res) {
+    res.json([
+        { id: 1, content: 'Hello1' },
+        { id: 2, content: 'Hello2' },
+        { id: 3, content: 'Hello3' },
+    ]);
+});
+app.post('/post', function (req, res) {
+    res.json([
+        { id: 1, content: 'post1' },
+        { id: 2, content: 'post2' },
+        { id: 3, content: 'post3' },
+    ]);
+});
+app.delete('/delete', function (req, res) {
+    res.json([
+        { id: 1, content: 'delete1' },
+        { id: 2, content: 'delete2' },
+        { id: 3, content: 'delete3' },
+    ]);
+});
+app.listen(3065, function () {
+    console.log('서버 실행 중');
 });
--- ch05/back/app.ts
+++ ch05/back/app.ts
@@ -1,16 +1,39 @@
-import * as http from 'http';
+import express, { Request, Response } from 'express';
 
-const server = http.createServer(
-  (req: http.IncomingMessage, res: http.ServerResponse) => {
-    console.log(req.url, req.method);
-    res.write('<h1>Hello Node</h1>');
-    res.write('<h2>Hello Node</h2>');
-    res.write('<h3>Hello Node</h3>');
-    res.write('<h4>Hello Node</h4>');
-    res.end('<h5>Hello node</h5>');
-  }
-);
+const app = express();
 
-server.listen(3065, () => {
-  console.log('3065 포트에서 서버 대기중');
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello express');
+});
+
+app.get('/api', (req: Request, res: Response) => {
+  res.send('Hello api');
+});
+
+app.get('/api/posts', (req: Request, res: Response) => {
+  res.json([
+    { id: 1, content: 'Hello1' },
+    { id: 2, content: 'Hello2' },
+    { id: 3, content: 'Hello3' },
+  ]);
+});
+
+app.post('/post', (req: Request, res: Response) => {
+  res.json([
+    { id: 1, content: 'post1' },
+    { id: 2, content: 'post2' },
+    { id: 3, content: 'post3' },
+  ]);
+});
+
+app.delete('/delete', (req: Request, res: Response) => {
+  res.json([
+    { id: 1, content: 'delete1' },
+    { id: 2, content: 'delete2' },
+    { id: 3, content: 'delete3' },
+  ]);
+});
+
+app.listen(3065, () => {
+  console.log('서버 실행 중');
 });
